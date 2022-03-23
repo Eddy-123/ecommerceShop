ee4df78b1a37f0d0249b064137f4aa34898bcb28
Eddy-123 <adegnandjoueddy12@gmail.com>
2022-03-23 18:21:40 +0100

    product detail page

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from store.models import Product
 
 def index(request):
@@ -7,4 +8,12 @@ def index(request):
         request,
         'store/index.html',
         context={'products': products}
+    )
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(
+        request,
+        'store/detail.html',
+        context={'product': product}
     )
